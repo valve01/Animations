2a945e80c77e906ad9782a667e9c5777c812e96b
Vikenty Mytko <118441831+valve01@users.noreply.github.com>
2024-03-04 14:42:47 +0300

    l

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+
+// import mobileNav from './modules/mobile-nav';
+import aboutSliderSwitcher from './modules/about-slider-switcher';
+
+import './modules/header-slider';
+import './modules/about-slider-inside';
+import './modules/about-slider-outside';
+import './modules/reviews-slider';
+// import './modules/jQueryModules/popup';
+// import './modules/jQueryModules/fullcalendar';
+// import './modules/jQueryModules/vizzu';
+// import './modules/jQueryModules/flowy';
+// import './modules/jQueryModules/uPlot';
+// import './modules/jQueryModules/g2';
+// import './modules/jQueryModules/zeu';
+// import './modules/jQueryModules/apexCharts';
+// import './modules/jQueryModules/gio';
+// import './modules/jQueryModules/tui';
+// import './modules/jQueryModules/datepicker';
+import './modules/svg-anim';
+
+
+
+
+// mobileNav();
+aboutSliderSwitcher()
+
---
+++ src/js/modules/jQueryModules/zeu.js
@@ -0,0 +1,7 @@
+import * as zeu from 'zeu'
+// import zeu from 'zeu'
+// Create a Zeu TextMeter.
+var textMeter = new zeu.TextMeter('text-meter');
+// Update display and percentage value.
+textMeter.displayValue = 'ZEU';
+textMeter.value = 80;
